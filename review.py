edd9df09611d298c5c17018bfd4922a2d62029de
Di Xu <xudi0428@163.com>
2022-02-28 20:37:18 +0800

    阶乘

---
+++ review.py
@@ -0,0 +1,17 @@
+# 输入M和N计算C(M,N)
+
+import re
+
+
+def main():
+    def multi(x):
+        sum = 1
+        for i in range(1,x+1):
+            sum *= i
+        return sum
+    m = int(input('m='))
+    n = int(input('n='))
+    print(multi(m)//multi(n)//multi(m-n))
+
+if __name__ == '__main__':
+    main()
